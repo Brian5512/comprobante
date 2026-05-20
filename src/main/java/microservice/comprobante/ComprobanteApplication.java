fa9d413a73ba19a3165f022f47fb21a9fe6afe8e
ElTioBrian <bri.bravom@duocuc.cl>
2026-05-20 15:39:19 -0400

    inicio comprobante

---
+++ src/main/java/microservice/comprobante/ComprobanteApplication.java
@@ -0,0 +1,13 @@
+package microservice.comprobante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprobanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprobanteApplication.class, args);
+	}
+
+}
