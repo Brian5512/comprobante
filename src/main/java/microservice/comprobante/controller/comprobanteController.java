9daf51f7becf9d66a6c98db11ea406206a082ad2
ElTioBrian <bri.bravom@duocuc.cl>
2026-05-23 17:55:06 -0400

    avanzanado en esta weaita

--- src/main/java/microservice/comprobante/controller/comprobanteController.java
+++ src/main/java/microservice/comprobante/controller/comprobanteController.java
@@ -3,15 +3,16 @@ package microservice.comprobante.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import microservice.comprobante.service.comprobanteService;
 
+import microservice.comprobante.service.comprobanteService;
 import microservice.comprobante.model.comprobante;
 
 @RestController
@@ -22,29 +23,15 @@ public class comprobanteController {
     private comprobanteService comprobanteService;
 
     @PostMapping()
-    public comprobante postComprobante(
-            @RequestBody comprobante comprobante) {
-
-        return comprobanteService.postComprobante(comprobante);
+    public ResponseEntity<comprobante> postComprobante(@RequestBody comprobante comprobante) {
+        try {
+            return new ResponseEntity<>(comprobanteService.postComprobante(comprobante),HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-
     @GetMapping()
-    public List<comprobante> getComprobante() {
-
-        return comprobanteService.getComprobante();
+    public ResponseEntity<List<comprobante>> getComprobante(){
+        return comprobanteService.findAll();
     }
-
-    @PutMapping("{id}")
-    public comprobante updateComprobante(
-            @PathVariable long id,
-            @RequestBody comprobante comprobante) {
-
-        return comprobanteService.updateComprobante(id, comprobante);
     }
-
-    @DeleteMapping("{id}")
-    public void deleteComprobante(@PathVariable long id) {
-
-        comprobanteService.deleteComprobante(id);
-    }
-}
